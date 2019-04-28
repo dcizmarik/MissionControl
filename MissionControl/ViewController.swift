7607db10b889141d4c813452a646fcdc8a6b4f5f
Daniel Cizmarik <dcizmarik@daniels-macbook-pro-3.local>
2019-04-28 14:23:19 -0400

    Initial Commit

---
+++ MissionControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MissionControl
+//
+//  Created by Daniel Cizmarik on 4/28/19.
+//  Copyright © 2019 Daniel Cizmarik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
